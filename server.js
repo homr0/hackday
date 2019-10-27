71072dde874ed936c694a6fc2a6610d8e6580729
Rebecca Hom <rebecca.hom001@gmail.com>
2019-10-26 18:34:55 -0700

    updated server to take in log of requests and output it as a text file

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const logger = require("morgan");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//  Creates a log file
+const logStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a"
+});
+
+logger.token("input", (req) => req.body.input);
+
+const logFormat = "[:date[web]]:method :url :input took :response-time ms to complete";
+
 // Express middleware
-app.use(logger("dev"));
+app.use(logger(logFormat, {
+  stream: logStream
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
